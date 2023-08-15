3603aabdf45366bc3585e04ac51ff3d69df9f095
WoodenMaiden <yann.pomie@laposte.net>
2023-08-15 19:27:59 +0700

    feat I,2
    Signed-off-by: WoodenMaiden <yann.pomie@laposte.net>

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,13 @@
 terraform {
-  
+  required_providers {
+    mongodbatlas = {
+      source = "mongodb/mongodbatlas"
+      version = "1.11.0"
+    }
+  }
+}
+
+provider "mongodbatlas" {
+    private_key = var.atlas_prv_key
+    public_key  = var.atlas_pub_key
 }
